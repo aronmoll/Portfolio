a643c9e021a01b7e475328a9d5cd3966b4e66d34
aronmoll <aronmoll6@gmail.com>
2024-03-17 13:44:02 +0100

    v1

---
+++ main.js
@@ -0,0 +1,13 @@
+// Import GSAP core
+import gsap from "gsap";
+
+// Import the plugins you intend to use
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Draggable from "gsap/Draggable";
+import Flip from "gsap/Flip";
+import MotionPathPlugin from "gsap/MotionPathPlugin";
+
+// Register the plugins with GSAP
+gsap.registerPlugin(ScrollTrigger, Draggable, Flip, MotionPathPlugin);
+
+
